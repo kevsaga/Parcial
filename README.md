8ff5656885f97d8baa4803bda00080ee637b18d4
kevsaga <kevsaga20@gmail.com>
2026-03-09 19:12:33 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Parcial
+# ParcialEstructurasDatosII
---
+++ Recursividad.java
@@ -0,0 +1,13 @@
+public class Recursividad {
+    public static String invertirCadena(String texto) {
+		// implementación aquí
+        return "";
+	}
+
+    public static void main(String[] args) {
+        String original = "Recursividad";
+        String invertida = invertirCadena(original);
+        System.out.println("Original: " + original);
+        System.out.println("Invertida: " + invertida);
+    }
+}
